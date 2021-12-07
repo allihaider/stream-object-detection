59b63bc6dee56f005bf141f175d78eb998e93fef
Ali Haider <ali.haider@redbuffer.net>
2021-12-07 23:37:40 +0500

    Listen to stream in separate process

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from collections import deque
 from stream_listener import StreamListener
+from multiprocessing import Process
+import time
 
 STREAM_URL = "http://localhost:8080"
 REATTEMPT_INTERVAL = 2
@@ -7,4 +9,11 @@ REATTEMPT_INTERVAL = 2
 frame_queue = deque(maxlen=30)
 
 stream_listener = StreamListener(STREAM_URL, REATTEMPT_INTERVAL)
-stream_listener.read_stream(frame_queue)
+stream_listening_process = Process(target=stream_listener.read_stream, args=(frame_queue,))
+stream_listening_process.start()
+
+while True:
+    print(len(frame_queue))
+# time.sleep(10)
+
+# stream_listener.read_stream(frame_queue)
