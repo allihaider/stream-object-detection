0d9bc66a1642972079125d561adbda8606c10fab
Ali Haider <ali.haider@redbuffer.net>
2021-12-17 15:33:24 +0500

    Fix formatting

--- model.py
+++ model.py
@@ -3,17 +3,24 @@ import torchvision
 import torch
 import torchvision.transforms as transforms
 import numpy as np
-import detect_utils
+
 
 class Model:
+    
     def __init__(self, class_names, detection_threshold, device):
+        
         self.class_names = class_names
         self.detection_threshold = detection_threshold
         self.device = device
         self.transform = transforms.Compose([transforms.ToTensor())
         self.model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
-
+        
+        return
+        
     def predict(self, images):
+        
+        inputs = []
+
         for image in images:
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             image = transform(image).to(device)
@@ -29,6 +36,7 @@ class Model:
         pred_labels = outputs[0]['labels'].cpu().numpy()
         
         threshold_mask = pred_scores >= self.detection_threshold
+        
         classes = np.array(pred_classes)[threshold_mask]
         boxes = pred_bboxes[threshold_mask].astype(np.int32)
         labels = pred_labels[threshold_mask]
